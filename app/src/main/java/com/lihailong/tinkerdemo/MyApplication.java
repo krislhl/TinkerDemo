8d81b9ff7fb293b649afe3fcd7291c18f79d9705
lihailong <lihailong@paifenle.com>
2017-07-03 10:03:20 +0800

    自定义Application

---
+++ app/src/main/java/com/lihailong/tinkerdemo/MyApplication.java
@@ -0,0 +1,16 @@
+package com.lihailong.tinkerdemo;
+
+import android.app.Application;
+
+/**
+ * 自定义Application
+ * @author lihailong
+ * @created Date 2017/7/3 10:01
+ **/
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
